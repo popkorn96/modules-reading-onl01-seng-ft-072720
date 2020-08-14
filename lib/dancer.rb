7d37aa245e1a294b9075c8bbb93a85f3329505f5
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-08-14 23:08:23 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,7 +1,9 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+require_relative './fancy_dance.rb'
 class Dancer
-
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
   include Dance
   extend MetaDancing
   attr_accessor :name
